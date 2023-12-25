7b0360af0a9e9192ac55978eea4da43ba5a760fc
shabnam <shabnam.estahbanati@gmail.com>
2023-12-25 05:30:47 -0800

    add js

---
+++ app.js
@@ -0,0 +1,39 @@
+const numbers = [
+    [0, "zero", 48],
+    [1, "one", 49],
+    [2, "two", 50],
+    [3, "three", 51],
+    [4, "four", 52],
+    [5, "five", 53],
+    [6, "six", 54],
+    [7, "seven", 55],
+    [8, "eight", 56],
+    [9, "nine", 57],
+    [".", "decimal", 190]
+  ];
+
+  const numpad = [
+    [0, "zero", 96],
+    [1, "one", 97],
+    [2, "two", 98],
+    [3, "three", 99],
+    [4, "four", 100],
+    [5, "five", 101],
+    [6, "six", 102],
+    [7, "seven", 103],
+    [8, "eight", 104],
+    [9, "nine", 105],
+    [".", "decimal", 190],
+    [".", "decimal", 110]
+  ];
+  const fractions = [
+    ["*", "multiply", 106],
+    ["+", "add", 107],
+    ["-", "subtract", 109],
+    ["/", "divide", 111]
+  ];
+  const resultButtons = [
+    ["AC", "clear", 46],
+    ["del", "delete", 8],
+    ["=", "equals", 13]
+  ];  
